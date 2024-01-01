9277b0122d5a65b4248f5e51cff1c3b6dc94de0e
Art Lasovsky <art@multi.app>
2024-01-01 12:34:59 +0100

    Gradient Refactoring

--- Sources/FoundationUI/Core/Gradient.swift
+++ Sources/FoundationUI/Core/Gradient.swift
@@ -9,59 +9,7 @@ import Foundation
 import SwiftUI
 
 public extension FoundationUI {
-    public struct Gradient: ShapeStyle {
-        public enum Point {
-            case top
-            case bottom
-            case leading
-            case trailing
-            case topLeading
-            case topTrailing
-            case bottomLeading
-            case bottomTrailing
-            
-            public var opposite: Self {
-                switch self {
-                case .top:
-                    return .bottom
-                case .bottom:
-                    return .top
-                case .leading:
-                    return .trailing
-                case .trailing:
-                    return .leading
-                case .topLeading:
-                    return .bottomTrailing
-                case .topTrailing:
-                    return .bottomLeading
-                case .bottomLeading:
-                    return .topTrailing
-                case .bottomTrailing:
-                    return .topLeading
-                }
-            }
-            public var unitPoint: UnitPoint {
-                switch self {
-                case .top:
-                    return .top
-                case .bottom:
-                    return .bottom
-                case .leading:
-                    return .leading
-                case .trailing:
-                    return .trailing
-                case .topLeading:
-                    return .topLeading
-                case .topTrailing:
-                    return .topTrailing
-                case .bottomLeading:
-                    return .bottomLeading
-                case .bottomTrailing:
-                    return .bottomTrailing
-                }
-            }
-        }
-        
+    struct Gradient: ShapeStyle {
         let colors: [FoundationUI.ColorScale]
         let swiftUIColors: [Color]
         let startPoint: Self.Point
@@ -90,7 +38,62 @@ public extension FoundationUI {
             return AnyShapeStyle(LinearGradient(
                 colors: colors,
                 startPoint: startPoint,
-                endPoint: endPoint))
+                endPoint: endPoint
+            ))
+        }
+    }
+}
+
+extension FoundationUI.Gradient {
+    public enum Point: Sendable {
+        case top
+        case bottom
+        case leading
+        case trailing
+        case topLeading
+        case topTrailing
+        case bottomLeading
+        case bottomTrailing
+        
+        public var opposite: Self {
+            switch self {
+            case .top:
+                return .bottom
+            case .bottom:
+                return .top
+            case .leading:
+                return .trailing
+            case .trailing:
+                return .leading
+            case .topLeading:
+                return .bottomTrailing
+            case .topTrailing:
+                return .bottomLeading
+            case .bottomLeading:
+                return .topTrailing
+            case .bottomTrailing:
+                return .topLeading
+            }
+        }
+        public var unitPoint: UnitPoint {
+            switch self {
+            case .top:
+                return .top
+            case .bottom:
+                return .bottom
+            case .leading:
+                return .leading
+            case .trailing:
+                return .trailing
+            case .topLeading:
+                return .topLeading
+            case .topTrailing:
+                return .topTrailing
+            case .bottomLeading:
+                return .bottomLeading
+            case .bottomTrailing:
+                return .bottomTrailing
+            }
         }
     }
 }
