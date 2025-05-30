bf887eb548d123f4dd9fcda00a12bb8fb6272701
Art Lasovsky <artlasovsky@gmail.com>
2025-05-30 19:18:00 +0200

    Added `Animation` variable

---
+++ Sources/FoundationUI/Theme/Variable/Animation.swift
@@ -0,0 +1,25 @@
+//
+//  Animation.swift
+//  FoundationUI
+//
+//  Created by Art Lasovsky on 30/05/2025.
+//
+
+import SwiftUI
+
+extension Theme {
+	@available(macOS 14.0, *)
+	@frozen
+	public struct Animation: FoundationVariableWithValue {
+		public typealias Result = SwiftUI.Animation
+		
+		public var value: Result
+		public var environmentAdjustment: EnvironmentAdjustment?
+		
+		public init() { self = .init(.default) }
+		
+		public init(value: Result) {
+			self.value = value
+		}
+	}
+}
